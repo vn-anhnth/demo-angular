187a69457cea566e19d01bb13c3af7d57360230e
vn-anhnth <anhnth@rakumo.vn>
2020-06-03 09:30:20 +0700

    add more services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+@Injectable({
+  providedIn: 'root',
+})
+
 export class AppComponent {
-  title = 'demo-angular';
+  title = 'Tour of Heroes';
 }
