470a67dbab134b7e914d158b5a59ea1e98583a87
vn-anhnth <anhnth@rakumo.vn>
2020-06-04 10:23:53 +0700

    Improve Service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,18 +12,12 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
 
-    heroes = HEROES;
-    selectedHero: Hero;
+    heroes: Hero[];
 
-    constructor(private heroService: HeroService, private messageService: MessageService) { }
+    constructor(private heroService: HeroService) { }
 
-    ngOnInit(): void {
+    ngOnInit() {
         this.getHeroes();
-    }
-
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-        this.messageService.add(`HeroService: Selected hero id = ${hero.id}`);
     }
 
     getHeroes(): void {
@@ -31,5 +25,18 @@ export class HeroesComponent implements OnInit {
             .subscribe(heroes => this.heroes = heroes);
     }
 
+    add(name: string): void {
+        name = name.trim();
+        if (!name) { return; }
+        this.heroService.addHero({ name } as Hero)
+            .subscribe(hero => {
+                this.heroes.push(hero);
+            });
+    }
+
+    delete(hero: Hero): void {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        this.heroService.deleteHero(hero).subscribe();
+    }
 
 }
